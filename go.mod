1a20f19480f5c33de5a09b9513d542f2d89c752c
Peikkin <respectrespect91@gmail.com>
2024-04-17 17:47:30 +0300

    end

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/Peikkin/video_service
 
 go 1.22.2
+
+require (
+	github.com/google/uuid v1.6.0
+	github.com/gorilla/mux v1.8.1
+	github.com/rs/zerolog v1.32.0
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,1 +1,94 @@
 package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+type Movie struct {
+	ID       uuid.UUID `json:"id"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+var movies []Movie
+
+func main() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/movies", createMovie).Methods("POST")
+	router.HandleFunc("/movies", getMovies).Methods("GET")
+	router.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	router.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	router.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	log.Info().Msg("Запуск сервера на порту :8080")
+	log.Fatal().Err(http.ListenAndServe(":8080", router)).Msg("Ошибка запуска сервера")
+
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "aplication/json")
+	var movie Movie
+	json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = uuid.New()
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "aplication/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID.String() == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID, _ = uuid.Parse(params["id"])
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			break
+		}
+	}
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "aplication/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID.String() == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "aplication/json")
+	params := mux.Vars(r)
+	for _, item := range movies {
+		if item.ID.String() == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "aplication/json")
+	json.NewEncoder(w).Encode(movies)
+}
